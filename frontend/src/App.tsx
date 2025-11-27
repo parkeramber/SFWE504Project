694cdd23957a4fe07cf8ecc42fa0ce5aeb0bc814
MartinEncinas <132398568+MartinEncinas@users.noreply.github.com>
2025-11-27 04:31:17 -0700

    Add files via upload

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,39 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import AuthPage from "./auth/AuthPage";
+import Dashboard from "./routes/Dashboard";
+import ProfilePage from "./routes/Profile";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import Navbar from "./components/Navbar";
+import AdminReports from "./routes/AdminReportsGUI";
 import "./App.css";
 
 export default function App() {
   return (
-    <div className="app-root">
-      <h1>Hello from UASAMS</h1>
-      <p>If you see this, React is working.</p>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<AuthPage defaultMode="login" />} />
+        <Route path="/register" element={<AuthPage defaultMode="register" />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/myprofile"
+          element={
+            <ProtectedRoute>
+              <ProfilePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/AdminReports" element={<AdminReports></AdminReports>} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </>
   );
 }
